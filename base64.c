fa969999abc47cfb01b93b00b63ef491e56984ef
Vincent Westenberg <vincent.westenberg@posteo.de>
2022-05-07 21:36:55 +0200

    Implement Base64 decode function

--- base64.c
+++ base64.c
@@ -37,3 +37,73 @@ size_t base64_encode(const uint8_t* input, size_t size, char* output) {
     }
     return 4 * ((size + 2) / 3) + 1;
 }
+
+#define INV_CHAR -1
+#define PAD_CHAR -2
+#define NUL_CHAR -3
+
+static int decode_map(char c) {
+    if (c >= 'A' && c <= 'Z') return c - 65;
+    if (c >= 'a' && c <= 'z') return c - 97 + 26;
+    if (c >= '0' && c <= '9') return c - 48 + 52;
+    if (c == '+') return 62;
+    if (c == '/') return 63;
+    if (c == '=') return PAD_CHAR;
+    if (c == '\0') return NUL_CHAR;
+    return INV_CHAR;
+}
+
+size_t base64_decode(const char* input, uint8_t* output, bool* error) {
+    size_t i = 0;
+    size_t size = 0;
+    int s, v;
+    bool pad;
+    uint8_t b1, b2, b3;
+    for (;;) {
+        s = i % 4;
+        v = decode_map(input[i++]);
+        if (v == INV_CHAR) goto error;
+        switch (s) {
+        case 0:
+            if (v == NUL_CHAR) goto done;
+            if (v == PAD_CHAR) goto error;
+            b1 = v << 2;
+            break;
+        case 1:
+            if (v == NUL_CHAR) goto error;
+            if (v == PAD_CHAR) goto error;
+            b1 |= (v & 0x30) >> 4;
+            b2 = (v & 0x0f) << 4;
+            break;
+        case 2:
+            if (v == NUL_CHAR) goto error;
+            pad = (v == PAD_CHAR);
+            if (!pad) {
+                b2 |= (v & 0x3c) >> 2;
+                b3 = (v & 0x03) << 6;
+            }
+            break;
+        case 3:
+            if (v == NUL_CHAR) goto error;
+            if (pad) {
+                if (v != PAD_CHAR) goto error;
+                b3 = 0x00;
+            } else {
+                b3 |= v;
+            }
+            if (output != NULL) {
+                *(output++) = b1;
+                *(output++) = b2;
+                *(output++) = b3;
+            }
+            size += 3;
+            break;
+        }
+    }
+done:
+    if (error != NULL) *error = false;
+    return size;
+error:
+    if (error != NULL) *error = true;
+    return 0;
+}
--- base64.h
+++ base64.h
@@ -1,11 +1,12 @@
 #ifndef BASE64_H
 #define BASE64_H
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
 size_t base64_encode(const uint8_t* input, size_t size, char* output);
 
-size_t base64_decode(const char* input, uint8_t* output);
+size_t base64_decode(const char* input, uint8_t* output, bool* error);
 
 #endif /* BASE64_H */
